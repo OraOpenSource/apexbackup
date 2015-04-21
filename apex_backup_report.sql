70ec2f04dcf5adbceb6ae70b9263f52d54cbb942
Martin D'Souza <martin.dsouza@gmail.com>
2015-04-21 08:50:14 -0600

    - Set termout off - Lowercase

--- apex_backup_report.sql
+++ apex_backup_report.sql
@@ -3,30 +3,31 @@
 
 -- http://forums.devshed.com/oracle-development-96/sql-plus-column-width-format-185085.html
 -- http://download.oracle.com/docs/cd/B10501_01/server.920/a90842/ch13.htm#1012748
-SET NUMFORMAT 9999999999999999999999999999999999999
-SET MARKUP HTML ON SPOOL ON HEAD "<title>APEX Export</title>"
+set numformat 9999999999999999999999999999999999999
+set markup html on spool on head "<title>APEX Export</title>"
+set termout off
 
-SET ECHO OFF
-SPOOL &1
+set echo off
+spool &1
 
 -- Current Date
 select to_char(sysdate, 'DD-MON-YYYY HH24:MI:SS') apex_backup_date
 from dual;
 
 -- Applications
-SELECT 'f' || application_id || '.sql' filename, application_id, workspace, application_name, owner
-FROM apex_applications
-WHERE workspace != 'INTERNAL'
-ORDER BY application_id;
+select 'f' || application_id || '.sql' filename, application_id, workspace, application_name, owner
+from apex_applications
+where workspace != 'INTERNAL'
+order by application_id;
 
 -- Workspaces
-SELECT 'f' || workspace_id || '.sql' filename, workspace
-FROM apex_workspaces
-WHERE workspace != 'INTERNAL'
-ORDER BY workspace_id;
+select 'f' || workspace_id || '.sql' filename, workspace
+from apex_workspaces
+where workspace != 'INTERNAL'
+order by workspace_id;
 
-SPOOL OFF
-SET MARKUP HTML OFF
-SET ECHO ON
+spool off
+set markup html off
+set echo on
 
-EXIT;
+exit;
